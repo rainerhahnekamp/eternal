5e3098bd0be21a38b22cc7b916fe4ab95738cdda
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2020-11-09 23:44:20 +0100

    add spring boot backend

---
+++ backend/src/main/java/com/rh/eternal/EternalApplication.java
@@ -0,0 +1,13 @@
+package com.rh.eternal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EternalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EternalApplication.class, args);
+	}
+
+}
