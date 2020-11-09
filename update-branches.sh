9314ae745f410a79904c421992eba94d0144bcdf
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2020-11-09 01:14:52 +0100

    update branch update

--- update-branches.sh
+++ update-branches.sh
@@ -12,10 +12,13 @@ git merge 02-unit-tests-a -m "merge from 02-unit-tests-a"
 git checkout 04-comp-tests
 git merge 03-unit-tests-b -m "merge from 03-unit-tests-b"
 
-git checkout 05-visual-regression
-git merge 04-comp-tests -m "merge from 04-comp-tests"
+git checkout 05-ngrx-tests
+git merge 04-comp-tests -m "merge from 04-compt-tests"
 
-git checkout 06-e2e
-git merge 05-visual-regression -m "merge from 05-visual-regression"
+git checkout 06-visual-regression
+git merge 05-ngrx-tests -m "merge from 05-ngrx-tests"
+
+git checkout 07-e2e
+git merge 06-visual-regression -m "merge from 06-visual-regression"
 
 git checkout master
