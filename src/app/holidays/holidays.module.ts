af6233b1b716c36aede13cfb5527e2962eea1213
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2020-12-13 00:00:49 +0100

    add more info page

--- src/app/holidays/holidays.module.ts
+++ src/app/holidays/holidays.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { RouterModule } from '@angular/router';
@@ -7,17 +8,23 @@ import { ReactiveComponentModule } from '@ngrx/component';
 import { StoreModule } from '@ngrx/store';
 import { holidaysFeatureKey, holidaysReducer } from './+state/holidays.reducer';
 import { HolidaysComponent } from './holidays/holidays.component';
+import { RequestInfoComponent } from './request-info/request-info.component';
 
 @NgModule({
-  declarations: [HolidaysComponent],
+  declarations: [HolidaysComponent, RequestInfoComponent],
   imports: [
     CommonModule,
     MatButtonModule,
     MatCardModule,
+    ReactiveFormsModule,
     RouterModule.forChild([
       {
         path: '',
         component: HolidaysComponent,
+      },
+      {
+        path: 'request-info/:holidayId',
+        component: RequestInfoComponent,
       },
     ]),
     StoreModule.forFeature(holidaysFeatureKey, holidaysReducer),
