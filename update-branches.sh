4f17dd69b1bc14d3c4616f4440d3dd1e10faf0fd
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2021-02-14 15:27:50 +0100

    remove rxjs test

--- update-branches.sh
+++ update-branches.sh
@@ -2,7 +2,7 @@ set -e
 
 git  checkout 01-setup
 
-branches=(01-setup 02-unit-tests-a 03-unit-tests-b 03-unit-tests-rxjs 04-comp-tests 04-comp-tests-material 04-comp-tests-harnesses 05-ngrx-tests 06-visual-regression 07-storybook 08-e2e)
+branches=(01-setup 02-unit-tests-a 03-unit-tests-b 04-comp-tests 04-comp-tests-material 04-comp-tests-harnesses 05-ngrx-tests 06-visual-regression 07-storybook 08-e2e)
 previous=
 current=
 
