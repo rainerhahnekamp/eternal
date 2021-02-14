9f23709140a444aa8d039e6b9613099df7353b7e
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2021-02-14 15:57:21 +0100

    next try for working update script

--- update-branches.sh
+++ update-branches.sh
@@ -2,7 +2,7 @@ set -e
 
 git  checkout 01-setup
 
-branches=(01-setup 02-unit-tests-a 03-unit-tests-b 04-comp-tests 04-comp-tests-material 04-comp-tests-harnesses 05-ngrx-tests 06-visual-regression 07-storybook 08-e2e)
+branches=(01-setup 02-unit-tests-a 03-unit-tests-b 03-unit-tests-rxjs 04-comp-tests 04-comp-tests-material 04-comp-tests-harnesses 05-ngrx-tests 06-visual-regression 07-storybook 08-e2e)
 previous=
 current=
 
@@ -14,7 +14,7 @@ for branch in ${branches[*]}; do
   if [ ! $current = "" ]
   then
     git checkout $current
-    git merge $previous $current -m merge
+    git merge $previous -m merge
   fi;
 done
 
