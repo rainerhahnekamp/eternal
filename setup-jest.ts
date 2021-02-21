f463abb67453b580e820a199514dbee6e9a88e66
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2021-02-21 22:33:48 +0100

    fix setup-jest.ts

--- setup-jest.ts
+++ setup-jest.ts
@@ -14,7 +14,11 @@ const mock = () => {
 Object.defineProperty(window, 'localStorage', { value: mock() });
 Object.defineProperty(window, 'sessionStorage', { value: mock() });
 Object.defineProperty(window, 'getComputedStyle', {
-  value: () => ['-webkit-appearance']
+  value: () => ({
+    getPropertyValue: (prop) => {
+      return '';
+    }
+  })
 });
 
 Object.defineProperty(document.body.style, 'transform', {
