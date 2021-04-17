afeabf5ceb87e01c7b7ccafb79e5995e77820a2a
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2021-04-17 14:36:16 +0200

    cypress with minimal setup and diary e2e based pom test

--- cypress/integration/global/holidays.spec.ts
+++
@@ -1,8 +0,0 @@
-describe('holidays', () => {
-  it('should stub the holidays', () => {
-    cy.intercept('GET', '**/holiday', { fixture: 'holidays.json' });
-    cy.visit('');
-    cy.getByAttr('btn-holidays').click();
-    cy.contains('Unicorn');
-  });
-});
--- cypress/support/api.ts
+++ cypress/support/api.ts
@@ -1,28 +1,6 @@
-import { BasicData } from '../../src/app/security/sign-up/basic/basic.component';
-import { DetailData } from '../../src/app/security/sign-up/detail/detail.component';
-import { InterestsData } from '../../src/app/security/sign-up/interests/interests.component';
-import { BaseApi } from './base-api';
+import {BaseApi} from "./base-api";
 
 class Api extends BaseApi {
-  signUp(basicData: BasicData, detailData: DetailData, interests: InterestsData) {
-    return this.post('security/sign-up', {
-      email: detailData.email,
-      password: detailData.password,
-      firstname: detailData.firstname,
-      name: detailData.name
-    }).then((response) => response.body as { userId: number });
-  }
-
-  signIn(email: string, password: string) {
-    return this.post('security/sign-in', {
-      email,
-      password
-    }).then((response) => response.body);
-  }
-
-  activate(userId: number, code: string) {
-    return this.post(`security/activate-user-by-code/${userId}/${code}`, {});
-  }
 }
 
 export const api = new Api();
