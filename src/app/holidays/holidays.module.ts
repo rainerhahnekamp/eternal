3d57921d889df0a990ac57a84215543535dd59e7
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2021-04-18 20:17:20 +0200

    remove import from ngrx component module

--- src/app/holidays/holidays.module.ts
+++ src/app/holidays/holidays.module.ts
@@ -4,14 +4,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { RouterModule } from '@angular/router';
-import { ReactiveComponentModule } from '@ngrx/component';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { HolidaysEffects } from './+state/holidays.effects';
 import { holidaysFeatureKey, holidaysReducer } from './+state/holidays.reducer';
+import { HolidayCardComponent } from './holiday-card/holiday-card.component';
 import { HolidaysComponent } from './holidays/holidays.component';
 import { RequestInfoComponent } from './request-info/request-info.component';
-import { HolidayCardComponent } from './holiday-card/holiday-card.component';
 
 @NgModule({
   declarations: [HolidaysComponent, RequestInfoComponent, HolidayCardComponent],
@@ -23,16 +22,15 @@ import { HolidayCardComponent } from './holiday-card/holiday-card.component';
     RouterModule.forChild([
       {
         path: '',
-        component: HolidaysComponent,
+        component: HolidaysComponent
       },
       {
         path: 'request-info/:holidayId',
-        component: RequestInfoComponent,
-      },
+        component: RequestInfoComponent
+      }
     ]),
     StoreModule.forFeature(holidaysFeatureKey, holidaysReducer),
-    EffectsModule.forFeature([HolidaysEffects]),
-    ReactiveComponentModule,
-  ],
+    EffectsModule.forFeature([HolidaysEffects])
+  ]
 })
 export class HolidaysModule {}
