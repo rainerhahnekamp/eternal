47fd8e7cf442710bcac9447fe69e1c02aee2d6b2
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2021-06-22 13:14:19 +0200

    fix sign-in

--- apps/eternal/src/app/security/signIn/sign-in.component.ts
+++ apps/eternal/src/app/security/signIn/sign-in.component.ts
@@ -6,8 +6,7 @@ import { UserService } from '../../shared/user.service';
 
 @Component({
   selector: 'app-sign-in',
-  templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  templateUrl: './sign-in.component.html'
 })
 export class SignInComponent {
   formGroup = new FormGroup({});
