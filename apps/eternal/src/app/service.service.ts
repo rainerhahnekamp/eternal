94466bef07ebf506c835b24ddf06aff9cffbace1
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2021-06-22 14:06:54 +0200

    fix vr tests

--- apps/eternal/src/app/service.service.ts
+++
@@ -1,9 +0,0 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class ServiceService {
-  constructor(private httpClient: HttpClient) {}
-}
