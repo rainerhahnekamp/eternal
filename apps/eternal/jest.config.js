ca1da82c5e05600fc15a5ade6aec826e7ada377b
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2021-06-23 15:51:13 +0200

    fix config for lodash support

--- apps/eternal/jest.config.js
+++ apps/eternal/jest.config.js
@@ -5,16 +5,17 @@ module.exports = {
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$',
-    },
+      stringifyContentPathRegex: '\\.(html|svg)$'
+    }
   },
   coverageDirectory: '../../coverage/apps/eternal',
   transform: {
-    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular'
   },
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
-    'jest-preset-angular/build/serializers/html-comment',
+    'jest-preset-angular/build/serializers/html-comment'
   ],
+  transformIgnorePatterns: ['node_modules/(?!lodash-es)']
 };
