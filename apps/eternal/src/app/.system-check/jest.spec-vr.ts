6ea021298831f5d6c08e1117f228848764cc0807
AndreHoltkamp <75220648+AndreHoltkamp@users.noreply.github.com>
2021-07-14 09:23:01 +0200

    Update jest.spec.vr.ts
    - add reachable link

--- apps/eternal/src/app/.system-check/jest.spec-vr.ts
+++ apps/eternal/src/app/.system-check/jest.spec-vr.ts
@@ -1,6 +1,6 @@
 describe('Jest with Puppeteer', () => {
   it('should run Jest with Puppeteer', async () => {
-    await page.goto('http://www.google.com');
+    await page.goto('http://www.datev.de');
     expect(true).toBe(true);
   });
 });
