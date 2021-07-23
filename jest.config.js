22c03bfce20cdaf7318a1e7c5c655e368d410690
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2021-07-23 11:34:04 +0200

    run migrations

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
-module.exports = {
-  projects: ['<rootDir>/apps/eternal'],
-};
+const { getJestProjects } = require('@nrwl/jest');
+
+module.exports = { projects: [...getJestProjects(), '<rootDir>/apps/eternal'] };
