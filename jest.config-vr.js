c46d292c2b74fd7e368fba52925e1c787fe07eeb
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2021-10-13 11:02:28 +0200

    fix puppeteer-vr config

--- jest.config-vr.js
+++ jest.config-vr.js
@@ -1,11 +1,11 @@
-const ts_preset = require('ts-jest/jest-preset');
 const puppeteer_preset = require('jest-puppeteer/jest-preset');
 
 const config = {
-  ...ts_preset,
-  ...puppeteer_preset,
+  globals: {
+    'ts-jest': {}
+  },
   testMatch: ['**/*.spec-vr.ts'],
-  setupFilesAfterEnv: ['<rootDir>/setup-jest-vr.ts']
+  ...puppeteer_preset,
+  setupFilesAfterEnv: ['./setup-jest-vr.ts']
 };
-
 module.exports = config;
