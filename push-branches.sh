a94a81a02d488700aee229e46d07bd0308b421ec
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2021-10-15 22:11:38 +0200

    update scripts

--- push-branches.sh
+++ push-branches.sh
@@ -1,6 +1,6 @@
 set -e
 
-git  checkout master
+git checkout master
 
 branches=(master 01-unit-tests-basic 02-unit-tests-advanced 03-rxjs-marbles 04-ngrx 05-comp-tests-basic 06-comp-tests-advanced 07-integration-tests 08-e2e-tests-basic 09-e2e-tests-advanced)
 
