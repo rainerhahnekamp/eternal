58f2edba75a018ccd775395c1e2fdbf664695833
AndreHoltkamp <75220648+AndreHoltkamp@users.noreply.github.com>
2021-11-10 09:26:43 +0100

    Create environment.bat
    add environment bat

---
+++ environment.bat
@@ -0,0 +1,5 @@
+set PUPPETEER_EXECUTABLE_PATH="C:\Program Files\Google\Chrome\Application\chrome.exe"
+set PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
+set PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=true
+set CYPRESS_DOWNLOAD_MIRROR=https://cypriote.pcfpub.dev.datev.de
+set CYPRESS_DOWNLOAD_USE_CA=1
