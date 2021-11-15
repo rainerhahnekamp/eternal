037942dfc8890efb054ecce9d9345aba7d885d7d
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2021-11-15 20:52:34 +0100

    fix activation component

--- apps/eternal/src/app/security/activate/activate.component.module.ts
+++ apps/eternal/src/app/security/activate/activate.component.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormlyModule } from '@ngx-formly/core';
