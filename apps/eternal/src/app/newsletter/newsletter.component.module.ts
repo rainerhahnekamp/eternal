372ed301baa083fe71ec5d753fc0471b7155e9a6
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-02-22 18:19:12 +0100

    add newsletter component

---
+++ apps/eternal/src/app/newsletter/newsletter.component.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NewsletterComponent } from './newsletter.component';
+
+@NgModule({
+  imports: [ReactiveFormsModule],
+  declarations: [NewsletterComponent]
+})
+export class NewsletterComponentModule {}
