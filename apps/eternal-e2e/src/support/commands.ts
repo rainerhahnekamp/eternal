07a16258dcd7d37c1b74f022af74bafb2697c33b
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-03-03 00:52:08 +0100

    add newsletter service and cypress command for testid

--- apps/eternal-e2e/src/support/commands.ts
+++ apps/eternal-e2e/src/support/commands.ts
@@ -1,7 +1,7 @@
 declare namespace Cypress {
   interface Chainable<Subject> {
-    getByAttr(selector: string): Chainable<JQuery<HTMLElement>>;
+    testid(selector: string): Chainable<JQuery<HTMLElement>>;
   }
 }
 
-Cypress.Commands.add('getByAttr', (selector: string) => cy.get(`[data-testid=${selector}]`));
+Cypress.Commands.add('testid', (selector: string) => cy.get(`[data-testid=${selector}]`));
---
+++ apps/eternal/src/app/newsletter/newsletter.service.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class NewsletterService {
+  constructor(private httpClient: HttpClient) {}
+
+  subscribe(email: string): Observable<boolean> {
+    return this.httpClient.post<boolean>('http://some.host.com/newsletter/subscribe', { email });
+  }
+}
