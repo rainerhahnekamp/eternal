ebb3a92744182253db31dd31bcadf4b4f465b09c
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-03-25 11:57:18 +0100

    replace dontleaveme directive with blinker

--- apps/eternal/src/app/holidays/holiday-card/holiday-card.component.module.ts
+++ apps/eternal/src/app/holidays/holiday-card/holiday-card.component.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { RouterModule } from '@angular/router';
-import { DontLeaveMeDirectiveModule } from '../../shared/dont-leave-me.directive';
+import { BlinkerDirectiveModule } from '../../shared/blinker.directive';
 import { HolidayCardComponent } from './holiday-card.component';
 
 @NgModule({
   declarations: [HolidayCardComponent],
   exports: [HolidayCardComponent],
-  imports: [MatCardModule, CommonModule, RouterModule, DontLeaveMeDirectiveModule, MatButtonModule]
+  imports: [MatCardModule, CommonModule, RouterModule, MatButtonModule, BlinkerDirectiveModule]
 })
 export class HolidayCardComponentModule {}
