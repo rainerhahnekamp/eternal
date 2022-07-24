7226143dc2df2e98df712317f5e4603332cc59e9
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-07-24 22:57:17 +0200

    add coverage config

--- jest.config.ts
+++ jest.config.ts
@@ -2,4 +2,13 @@ import { getJestProjects } from '@nrwl/jest';
 
 export default {
   projects: getJestProjects(),
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
+  collectCoverageFrom: ['**/src/app/**']
 };
