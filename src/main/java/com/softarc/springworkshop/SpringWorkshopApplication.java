463836175ce3aeab07f80e4c3c98981891d377d1
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-11-04 18:43:12 +0100

    init

---
+++ src/main/java/com/softarc/springworkshop/SpringWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.softarc.springworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWorkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/softarc/springworkshop/SpringWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softarc.springworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
