92137790626b828a61426d90820f161c9d00e5d2
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-11-14 17:26:54 +0100

    add solution

---
+++ src/main/java/com/softarc/eternal/data/FsHolidaysRepository.java
@@ -0,0 +1,102 @@
+package com.softarc.eternal.data;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.softarc.eternal.domain.Holiday;
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import lombok.SneakyThrows;
+import lombok.extern.java.Log;
+
+@Log
+public class FsHolidaysRepository implements HolidaysRepository {
+
+  private final ObjectMapper objectMapper;
+  private final List<Holiday> holidays;
+
+  private Long currentId;
+  private final File file;
+
+  @SneakyThrows
+  public FsHolidaysRepository(ObjectMapper objectMapper, String filename) {
+    this.objectMapper = objectMapper;
+    file = Paths.get(filename).toFile();
+
+    if (!this.file.exists()) {
+      this.holidays = new ArrayList<>();
+      this.init();
+    } else {
+      this.holidays =
+        new ArrayList<>(
+          Arrays.asList(this.objectMapper.readValue(this.file, Holiday[].class))
+        );
+
+      this.currentId = getCurrentId();
+    }
+  }
+
+  private Long getCurrentId() {
+    return this.holidays.stream()
+      .map(Holiday::getId)
+      .max(Long::compareTo)
+      .orElse(0L);
+  }
+
+  private void init() {
+    this.holidays.clear();
+    holidays.add(new Holiday(1L, "Canada", "Visit Rocky Mountains"));
+    holidays.add(new Holiday(2L, "China", "To the Middle Kingdom"));
+    this.currentId = this.getCurrentId();
+    this.persist();
+  }
+
+  @SneakyThrows
+  private void persist() {
+    this.objectMapper.writeValue(this.file, this.holidays);
+  }
+
+  @Override
+  public List<Holiday> findAll() {
+    return this.holidays;
+  }
+
+  @Override
+  public void add(String name) {
+    this.holidays.add(new Holiday(++this.currentId, name, "-"));
+    this.persist();
+    FsHolidaysRepository.log.info(String.format("Holiday %s was added", name));
+  }
+
+  @Override
+  public Optional<Holiday> find(Long id) {
+    for (Holiday holiday : this.holidays) {
+      if (holiday.getId().equals(id)) {
+        return Optional.of(holiday);
+      }
+    }
+
+    return Optional.empty();
+  }
+
+  @Override
+  public void remove(Long id) {
+    this.find(id)
+      .ifPresentOrElse(
+        holiday -> {
+          this.holidays.remove(holiday);
+          this.persist();
+          FsHolidaysRepository.log.info(
+            String.format("Holiday %s was removed", holiday.getName())
+          );
+        },
+        () -> {
+          throw new RuntimeException(
+            String.format("could not find Holiday with id %d", id)
+          );
+        }
+      );
+  }
+}
