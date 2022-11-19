adc9e7ee2f7f324fbe2f11f758aa2ea0a2e606e9
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-11-19 15:56:21 +0100

    add draft for holidays

---
+++ libs/admin/holidays/feature/src/lib/admin-holidays-feature.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { HolidaysComponent } from '../../../ui/src/lib/holidays/holidays.component';
+
+export const adminHolidaysRoutes: Routes = [
+  { path: '', component: HolidaysComponent },
+];
