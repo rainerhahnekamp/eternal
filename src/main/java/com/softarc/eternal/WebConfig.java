e55e4468f0d4a7b65abdcf40cb6b6557ac2a0a8e
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-11-22 22:28:43 +0100

    connect the frontend

--- src/main/java/com/softarc/eternal/WebConfig.java
+++ src/main/java/com/softarc/eternal/WebConfig.java
@@ -5,19 +5,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig {
 
-  @Configuration(proxyBeanMethods = false)
-  public class MyCorsConfiguration {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-      return new WebMvcConfigurer() {
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-          registry.addMapping("/api/**");
-        }
-      };
-    }
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry
+          .addMapping("/api/**")
+          .allowedOrigins("http://localhost:4200")
+          .allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE")
+          .allowCredentials(true);
+      }
+    };
   }
 }
