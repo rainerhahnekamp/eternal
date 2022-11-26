ec9a7447e763025331debc62b8ad076a88366370
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-11-26 22:40:39 +0100

    update

--- labs/db/ddl-holidays.sql
+++ labs/db/ddl-holidays.sql
@@ -1,29 +1,29 @@
-create table Holiday
+create table HOLIDAY
 (
-  Id          int auto_increment primary key,
-  Name        nvarchar(50) not null,
-  Description text not null,
-  CoverPath       text null
+  ID          int auto_increment primary key,
+  NAME        nvarchar(50) not null,
+  DESCRIPTION text not null,
+  COVER_PATH       text null
 );
 
-create table Guide
+create table GUIDE
 (
-  Id        int auto_increment primary key,
-  Firstname nvarchar(50) not null,
-  Lastname  nvarchar(50) not null,
-  Email     nvarchar(50) not null,
-  PhoneNr   nvarchar(50) not null,
-  Bio       text not null
+  ID        int auto_increment primary key,
+  FIRSTNAME nvarchar(50) not null,
+  LASTNAME  nvarchar(50) not null,
+  EMAIL     nvarchar(50) not null,
+  PHONE_NR   nvarchar(50) not null,
+  BIO       text not null
 );
 
-create table HolidayTrip
+create table HOLIDAY_TRIP
 (
-  Id              int auto_increment primary key,
-  FromDate        timestamp,
-  ToDate          timestamp,
-  PriceSingleRoom decimal(7, 2),
-  PriceDoubleRoom decimal(7, 2),
-  Currency        nvarchar(3),
-  HolidayId       int references Holiday (Id),
-  GuideId         int references Guide (Id)
+  ID              int auto_increment primary key,
+  FROM_DATE        timestamp,
+  TO_DATE          timestamp,
+  PRICE_SINGLE_ROOM decimal(7, 2),
+  PRICE_DOUBLE_ROOM decimal(7, 2),
+  CURRENCY        nvarchar(3),
+  HOLIDAY_ID       int references Holiday (Id),
+  GUIDE_ID         int references Guide (Id)
 );
