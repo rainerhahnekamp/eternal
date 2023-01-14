6bb35ebd7d3d78c23ee0b2d6cdbbef6608359762
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-01-14 23:15:18 +0100

    change to main branch

--- push-branches.sh
+++ push-branches.sh
@@ -1,12 +1,12 @@
 set -e
 
-git checkout master
+git checkout main
 
-branches=(master solution-1-e2e-basics solution-2-e2e-advanced solution-3-unit-tests-async-mock solution-4-rxjs-ngrx solution-5-component-integration-tests solution-6-component-integration-tests-fallback solution-7-visual-regression)
+branches=(main solution-1-e2e-basics solution-2-e2e-advanced solution-3-unit-tests-async-mock solution-4-rxjs-ngrx solution-5-component-integration-tests solution-6-component-integration-tests-fallback solution-7-visual-regression)
 
 for branch in ${branches[*]}; do
   git checkout $branch
   git push $1 $branch
 done
 
-git checkout master
+git checkout main
--- update-branches.sh
+++ update-branches.sh
@@ -1,8 +1,8 @@
 set -e
 
-git checkout master
+git checkout main
 
-branches=(master solution-1-e2e-basics solution-2-e2e-advanced solution-3-unit-tests-async-mock solution-4-rxjs-ngrx solution-5-component-integration-tests solution-6-component-integration-tests-fallback solution-7-visual-regression)
+branches=(main solution-1-e2e-basics solution-2-e2e-advanced solution-3-unit-tests-async-mock solution-4-rxjs-ngrx solution-5-component-integration-tests solution-6-component-integration-tests-fallback solution-7-visual-regression)
 previous=
 current=
 
@@ -22,4 +22,4 @@ done
 git checkout $branch
 git merge $current $branch -m merge
 
-git checkout master
+git checkout main
