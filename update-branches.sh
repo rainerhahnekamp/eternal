e574e9e24be58a98e0135b9d4038733e40082915
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-03-05 19:11:07 +0100

    add starter for lab 2

--- update-branches.sh
+++ update-branches.sh
@@ -2,7 +2,7 @@ set -e
 
 git checkout main
 
-branches=(main solution-1-e2e-basics solution-2-e2e-advanced solution-3-unit-tests-async-mock solution-4-rxjs-ngrx solution-5-component-integration-tests solution-6-component-integration-tests-fallback solution-7-visual-regression)
+branches=(main solution-1-e2e-basics lab-2-starter solution-2-e2e-advanced solution-3-unit-tests-async-mock solution-4-rxjs-ngrx solution-5-component-integration-tests solution-6-component-integration-tests-fallback solution-7-visual-regression)
 previous=
 current=
 
