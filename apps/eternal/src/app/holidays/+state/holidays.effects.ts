38e0e9f0b3e04065ede638c4fc866e42dae44e8e
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-03-05 14:01:21 +0100

    replace holiday effect with functional effect

--- apps/eternal/src/app/holidays/+state/holidays.effects.ts
+++ apps/eternal/src/app/holidays/+state/holidays.effects.ts
@@ -1,28 +1,28 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { map, switchMap } from 'rxjs/operators';
 import { Holiday } from '../holiday';
 import { holidaysActions } from './holidays.actions';
 import { Configuration } from '../../shared/configuration';
 
-@Injectable()
-export class HolidaysEffects {
-  #actions$ = inject(Actions);
-  #httpClient = inject(HttpClient);
-  #baseUrl = inject(Configuration).baseUrl;
-
-  find$ = createEffect(() =>
-    this.#actions$.pipe(
+export const findHolidays = createEffect(
+  (
+    actions$ = inject(Actions),
+    httpClient = inject(HttpClient),
+    baseUrl = inject(Configuration).baseUrl
+  ) => {
+    return actions$.pipe(
       ofType(holidaysActions.findHolidays),
-      switchMap(() => this.#httpClient.get<Holiday[]>('/holiday')),
+      switchMap(() => httpClient.get<Holiday[]>('/holiday')),
       map((holidays) =>
         holidays.map((holiday) => ({
           ...holiday,
-          imageUrl: `${this.#baseUrl}${holiday.imageUrl}`
+          imageUrl: `${baseUrl}${holiday.imageUrl}`
         }))
       ),
       map((holidays) => holidaysActions.findHolidaysSuccess({ holidays }))
-    )
-  );
-}
+    );
+  },
+  { functional: true }
+);
--- apps/eternal/src/app/holidays/holidays.routes.ts
+++ apps/eternal/src/app/holidays/holidays.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { provideEffects } from '@ngrx/effects';
 import { provideState } from '@ngrx/store';
-import { HolidaysEffects } from './+state/holidays.effects';
+import { findHolidays } from './+state/holidays.effects';
 import { holidaysFeature } from './+state/holidays.reducer';
 import { HolidaysComponent } from './holidays/holidays.component';
 import { RequestInfoComponent } from './request-info/request-info.component';
@@ -9,7 +9,7 @@ import { RequestInfoComponent } from './request-info/request-info.component';
 const holidayRoutes: Routes = [
   {
     path: '',
-    providers: [provideState(holidaysFeature), provideEffects(HolidaysEffects)],
+    providers: [provideState(holidaysFeature), provideEffects({ holidays: findHolidays })],
     component: HolidaysComponent
   },
   {
