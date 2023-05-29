3f0f88ab7bc5566b91a707326a7cca3504729cf8
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-05-29 20:44:41 +0200

    update scripts

--- update-branches.sh
+++ update-branches.sh
@@ -2,7 +2,7 @@ set -e
 
 git checkout main
 
-branches=(main solution-1-e2e-basics lab-2-starter solution-2-e2e-advanced solution-3-async-mock-constructor solution-3-async-mock-inject solution-4-rxjs-ngrx lab-5-starter solution-5-component-integration-tests solution-6-component-integration-tests-fallback solution-7-visual-regression)
+branches=(main solution-01-e2e-basics starter-02-e2e-advanced solution-02-e2e-advanced starter-03-component-integration-tests solution-03-component-integration-tests)
 previous=
 current=
 
