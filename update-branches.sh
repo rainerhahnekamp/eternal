72e8ec6f9c79b359a8390fdc5712137de115f499
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-06-07 20:33:36 +0200

    update update branches script

--- update-branches.sh
+++ update-branches.sh
@@ -6,9 +6,9 @@ branches=(
   main
   solution-01-e2e-basics
   starter-02-e2e-advanced solution-02-e2e-advanced
-  starter-03-component-integration-tests solution-03-component-integration-tests
-  starter-04-unit-tests-async-mock solution-04-unit-tests-async-mock solution-04-unit-tests-async-mock-inject
-  starter-05-rxjs solution-05-rxjs
+  starter-03-unit-tests-async-mock solution-03-unit-tests-async-mock solution-03-unit-tests-async-mock-inject
+  starter-04-rxjs solution-04-rxjs
+  starter-05-component-integration-tests solution-05-component-integration-tests
   starter-06-component-integration-tests-fallback solution-06-component-integration-tests-fallback
 )
 previous=
