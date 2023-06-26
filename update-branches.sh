75c7352679a418e212add0d077c8997b9671e390
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-06-27 01:27:39 +0200

    update scripts

--- update-branches.sh
+++ update-branches.sh
@@ -6,7 +6,7 @@ branches=(
   main
   solution-01-e2e-basics
   starter-02-e2e-advanced solution-02-e2e-advanced
-  starter-03-unit-tests-async-mock solution-03-unit-tests-async-mock solution-03-unit-tests-async-mock-inject
+  starter-03-unit-tests-async-mock solution-03-unit-tests-async-mock
   starter-04-rxjs solution-04-rxjs
   starter-05-component-integration-tests solution-05-component-integration-tests
   starter-06-component-integration-tests-fallback solution-06-component-integration-tests-fallback
