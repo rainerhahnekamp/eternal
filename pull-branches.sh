9d5732fe3795e79e7bfb578ac82cfc7ba88ed971
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-07-20 08:49:07 +0200

    pull branches

---
+++ pull-branches.sh
@@ -0,0 +1,20 @@
+set -e
+
+git checkout main
+
+branches=(
+  main
+  solution-1-1-basics solution-1-2-advanced
+  solution-2-1-basics solution-2-2-advanced
+  solution-3-1-basics solution-3-2-openapi
+  solution-3-3-upload-download-1-basic solution-3-3-upload-download-2-download solution-3-3-upload-download-3-misc
+  solution-4-data-1-h2 solution-4-data-2-entity solution-4-data-3-testing solution-4-data-4-relations solution-4-data-5-mysql-flyway
+  solution-5-microservices-1-brochure-status solution-5-microservices-2-web-client solution-5-microservices-3-feign solution-5-microservices-4-amqp
+)
+
+for branch in ${branches[*]}; do
+  git checkout $branch
+  git pull origin
+done
+
+git checkout main
