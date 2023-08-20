e2e2ab4096ea5d58e1dd55b170ba0951a046422b
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-08-20 11:39:14 +0200

    add holiday cards tory

--- src/app/holidays/holiday-card/holiday-card.component.stories.ts
+++ src/app/holidays/holiday-card/holiday-card.component.stories.ts
@@ -1,33 +1,45 @@
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { RouterTestingModule } from '@angular/router/testing';
-import { Meta, moduleMetadata } from '@storybook/angular';
+import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
 import { HolidayCardComponent } from './holiday-card.component';
+import { ActivatedRoute } from '@angular/router';
+import { Holiday } from '@app/holidays/model';
 
 const meta: Meta<HolidayCardComponent> = {
   title: 'Eternal/Holiday Card',
   component: HolidayCardComponent,
   decorators: [
     moduleMetadata({
-      imports: [MatButtonModule, MatCardModule, RouterTestingModule],
+      providers: [{ provide: ActivatedRoute, useValue: undefined }],
+      imports: [MatButtonModule, MatCardModule],
     }),
   ],
 };
 
 export default meta;
+type Story = StoryObj<HolidayCardComponent>;
 
-export const Default = () => ({
-  props: {
-    holiday: {
-      id: 1,
-      title: 'Welcome',
-      description: 'Welcome to the Angular Testing Workshop',
-      imageUrl: 'https://avatars.githubusercontent.com/u/61938637?s=200&v=4',
-      teaser: '',
-      minCount: 8,
-      maxCount: 17,
-      typeId: 1,
-      durationInDays: 10,
-    },
-  },
+const defaultHoliday: Holiday = {
+  id: 1,
+  title: 'Vienna',
+  description: 'Holiday in Vienna',
+  imageUrl: 'vienna.jpg',
+  teaser: '',
+  minCount: 8,
+  maxCount: 17,
+  typeId: 1,
+  durationInDays: 10,
+  soldOut: false,
+  onSale: false,
+};
+
+const createStory = (
+  holiday: Partial<Holiday> = {},
+  name?: string,
+): StoryObj<HolidayCardComponent> => ({
+  name,
+  render: () => ({ props: { holiday: { ...defaultHoliday, ...holiday } } }),
 });
+
+export const Default = createStory({}, 'Vienna');
+export const Empty = createStory({ title: '', description: '' }, 'Vienna');
