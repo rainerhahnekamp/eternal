10bbcfb5da6f22b0a43be845a51c388f22822c96
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-08-20 13:14:25 +0200

    add vr for customers

--- tests/visual-regression.spec.ts
+++ tests/visual-regression.spec.ts
@@ -1,11 +1,36 @@
-import { expect, test } from '@playwright/test';
+import { expect, test as base } from '@playwright/test';
+import { ShellFixtures, shellFixtures } from './fixtures/shell.fixtures';
+import {
+  CustomersFixtures,
+  customersFixtures,
+} from './fixtures/customer.fixtures';
+
+const test = base.extend<ShellFixtures & CustomersFixtures>({
+  ...shellFixtures,
+  ...customersFixtures,
+});
 
 test.describe('Visual Regression', () => {
   test.describe('Application', () => {
-    test('home screenshot', async ({ page }) => {
+    test.beforeEach(async ({ page }) => {
       await page.goto('');
+    });
+
+    test('home screenshot', async ({ page }) => {
       await page.getByRole('heading', { name: 'Welcome to Eternal' }).waitFor();
       await expect(page).toHaveScreenshot('home.png');
+    });
+
+    test('customers', async ({ page, sidemenuPage, customersPage }) => {
+      await page.setViewportSize({ width: 1300, height: 430 });
+      await sidemenuPage.select('Customers');
+      await customersPage.rowsLocator.first().waitFor();
+      await expect(page).toHaveScreenshot('customers.png', {
+        fullPage: true,
+        omitBackground: true,
+        mask: [page.locator('footer')],
+        maskColor: 'white',
+      });
     });
   });
 });
