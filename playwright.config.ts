bebd19249320df68adb7f83fed593c2579ac5b77
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-08-21 22:30:09 +0200

    new github actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI ? 'blog' : 'html',
 
   globalSetup: './tests/scripts/global-setup.ts',
   globalTeardown: './tests/scripts/global-teardown.ts',
