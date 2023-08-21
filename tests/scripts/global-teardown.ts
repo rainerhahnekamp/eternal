c218d6e71e864f9e7ae7a3e41f14a7a0e1944e96
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-08-21 21:59:35 +0200

    fix linting

--- tests/scripts/global-teardown.ts
+++ tests/scripts/global-teardown.ts
@@ -1,6 +1,5 @@
 import Nyc from 'nyc';
 import * as path from 'path';
-import * as fs from 'fs/promises';
 
 export default async function createCoverageReport() {
   const coverageDir = path.join(process.cwd(), 'nyc');
