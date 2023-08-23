8c5e93bf20499f335d8b62b4e03f9f5f390464f3
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-08-23 09:42:31 +0200

    add solution

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,18 +1,16 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  
   e2e: {
-    'baseUrl': 'http://localhost:4200'
+    baseUrl: 'http://localhost:4200',
+    experimentalWebKitSupport: true,
   },
-  
-  
+
   component: {
     devServer: {
       framework: 'angular',
       bundler: 'webpack',
     },
-    specPattern: '**/*.cy.ts'
-  }
-  
-})
+    specPattern: '**/*.cy.ts',
+  },
+});
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -2,5 +2,10 @@
 declare namespace Cypress {
   interface Chainable<Subject> {
     findByRole(role: string, textMatch: string | RegExp): Chainable<JQuery>;
+    testid(selector: string): Chainable<JQuery>;
   }
 }
+
+Cypress.Commands.add('testid', (selector) =>
+  cy.get(`[data-testid=${selector}]`),
+);
