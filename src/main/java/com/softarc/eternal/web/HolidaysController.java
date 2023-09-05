5b6c40a81091118257f49daacf634c288302fd8e
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-09-06 00:21:59 +0200

    add mapper

--- src/main/java/com/softarc/eternal/web/HolidaysController.java
+++ src/main/java/com/softarc/eternal/web/HolidaysController.java
@@ -1,37 +1,38 @@
 package com.softarc.eternal.web;
 
 import com.softarc.eternal.data.HolidaysRepository;
-import com.softarc.eternal.domain.Holiday;
 import com.softarc.eternal.web.exception.IdNotFoundException;
+import com.softarc.eternal.web.mapping.HolidaysMapper;
 import com.softarc.eternal.web.request.HolidayDto;
 import com.softarc.eternal.web.response.HolidayResponse;
-import java.util.List;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RequestMapping("/api/holidays")
 @RestController
 public class HolidaysController {
 
   private final HolidaysRepository repository;
+  private final HolidaysMapper holidaysMapper;
 
-  public HolidaysController(HolidaysRepository repository) {
+  public HolidaysController(HolidaysRepository repository, HolidaysMapper holidaysMapper) {
     this.repository = repository;
+    this.holidaysMapper = holidaysMapper;
   }
 
   @GetMapping
   public List<HolidayResponse> index() {
     return this.repository.findAll()
       .stream()
-      .map(this::toHolidayResponse)
+      .map(holidaysMapper::holidayToResponse)
       .toList();
   }
 
   @GetMapping("{id}")
   public HolidayResponse find(@PathVariable("id") Long id) {
     return this.repository.find(id)
-      .map(this::toHolidayResponse)
+      .map(holidaysMapper::holidayToResponse)
       .orElseThrow(IdNotFoundException::new);
   }
 
@@ -52,13 +53,5 @@ public class HolidaysController {
   @DeleteMapping("{id}")
   public void remove(@PathVariable("id") Long id) {
     this.repository.remove(id);
-  }
-
-  private HolidayResponse toHolidayResponse(Holiday holiday) {
-    return new HolidayResponse(
-      holiday.getId(),
-      holiday.getName(),
-      holiday.getDescription()
-    );
   }
 }
