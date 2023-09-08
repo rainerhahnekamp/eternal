a393b2b927337395cec0f4eac618e5c86b91f8cc
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-09-08 12:48:39 +0200

    remove trips from response

--- src/main/java/com/softarc/eternal/web/response/HolidayResponse.java
+++ src/main/java/com/softarc/eternal/web/response/HolidayResponse.java
@@ -1,12 +1,9 @@
 package com.softarc.eternal.web.response;
 
-import com.softarc.eternal.domain.HolidayTrip;
 import jakarta.validation.constraints.NotNull;
-import java.util.List;
 
 public record HolidayResponse(
   @NotNull Long id,
   @NotNull String name,
-  @NotNull String description,
-  @NotNull List<HolidayTrip> holidayTrips
+  @NotNull String description
 ) {}
