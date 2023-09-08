781784688c1f6797dfa6ef90e2c8a07f550d54b6
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-09-08 12:21:57 +0200

    improve test

--- src/test/java/com/softarc/eternal/web/HolidaysControllerUnitTest.java
+++ src/test/java/com/softarc/eternal/web/HolidaysControllerUnitTest.java
@@ -11,10 +11,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles
 public class HolidaysControllerUnitTest {
 
   @Autowired
