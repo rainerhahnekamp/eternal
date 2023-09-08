64b441e4d916d2252ee66caa7db63de9d9966f18
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-09-08 13:09:47 +0200

    fix response

--- src/main/java/com/softarc/eternal/web/response/HolidayResponse.java
+++ src/main/java/com/softarc/eternal/web/response/HolidayResponse.java
@@ -1,9 +1,10 @@
 package com.softarc.eternal.web.response;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record HolidayResponse(
   @NotNull Long id,
-  @NotNull String name,
-  @NotNull String description
+  @NotBlank String name,
+  @NotBlank String description
 ) {}
