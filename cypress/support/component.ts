79ce2f706fc78138149124c0b3c361198f72e9ff
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-09-12 18:09:28 +0200

    add solution

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -1,36 +1,11 @@
-// ***********************************************************
-// This example support/component.ts is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
-
-// Import commands.js using ES2015 syntax:
-import './commands';
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
 import { mount } from 'cypress/angular';
 import Chainable = Cypress.Chainable;
 
-// Augment the Cypress namespace to include type definitions for
-// your custom command.
-// Alternatively, can be defined in cypress/support/component.d.ts
-// with a <reference path="./component" /> at the top of your spec.
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      mount: typeof mount;
-      testid(testid: string): Chainable<JQuery>;
-    }
+// eslint-disable-next-line @typescript-eslint/no-namespace
+declare namespace Cypress {
+  interface Chainable {
+    mount: typeof mount;
+    testid(testid: string): Chainable<JQuery>;
   }
 }
 
