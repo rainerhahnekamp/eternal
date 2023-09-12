fbe5ca98b846ea00155d7d102b28d9901d12a382
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-09-12 16:44:20 +0200

    fix cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,17 +1,16 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  component: {
-    devServer: {
-      framework: "angular",
-      bundler: "webpack",
-    },
-    specPattern: "**/*.cy.ts",
+  e2e: {
+    baseUrl: 'http://localhost:4200',
+    experimentalWebKitSupport: true,
   },
 
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+  component: {
+    devServer: {
+      framework: 'angular',
+      bundler: 'webpack',
     },
+    specPattern: '**/*.cy.ts',
   },
 });
