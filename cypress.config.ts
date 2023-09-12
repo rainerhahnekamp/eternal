c009c1bfa9f42f8d44185a277c493effa379290f
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-09-12 16:39:11 +0200

    improve init

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'cypress';
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   component: {
     devServer: {
-      framework: 'angular',
-      bundler: 'webpack',
+      framework: "angular",
+      bundler: "webpack",
     },
-    specPattern: '**/*.cy.ts',
+    specPattern: "**/*.cy.ts",
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
 });
