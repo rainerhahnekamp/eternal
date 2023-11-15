afc9ae8baf69702a6566e6919e80381cee143801
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-11-15 21:32:47 +0100

    feat: add solution

--- src/app/bookings/overview/overview.component.ts
+++ src/app/bookings/overview/overview.component.ts
@@ -1,36 +1,21 @@
-import { Component, effect, inject, OnInit } from '@angular/core';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Component, Input } from '@angular/core';
 import { Booking } from '../+state/bookings.reducer';
-import { DatePipe } from '@angular/common';
-import { BookingsRepository } from '@app/bookings/+state/bookings-repository.service';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { CommonModule } from '@angular/common';
+
+export interface ViewModel {
+  bookings: Booking[];
+  customerName: string;
+}
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   standalone: true,
-  imports: [MatTableModule, DatePipe],
+  imports: [MatTableModule, CommonModule],
 })
-export class OverviewComponent implements OnInit {
-  userName = '';
+export class OverviewComponent {
+  @Input() viewModel: ViewModel | undefined;
   displayedColumns = ['holidayId', 'date', 'status', 'comment'];
   dataSource = new MatTableDataSource<Booking>([]);
-
-  #repo = inject(BookingsRepository);
-
-  constructor() {
-    effect(() => {
-      const bookingData = this.#repo.bookingsData();
-      if (!bookingData) {
-        return;
-      }
-      if (bookingData.loaded) {
-        this.userName = bookingData.customerName;
-        this.dataSource.data = bookingData.bookings;
-      }
-    });
-  }
-
-  ngOnInit(): void {
-    this.#repo.load();
-  }
 }
