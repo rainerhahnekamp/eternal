d626d6759d8f2d4d18ea83056ad631144a069599
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-11-28 15:57:19 +0100

    feat: add controller advice

--- src/main/java/com/softarc/eternal/common/GlobalControllerAdvice.java
+++ src/main/java/com/softarc/eternal/common/GlobalControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.softarc.eternal.common;
 
+import com.softarc.eternal.customer.web.exception.CustomerException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,5 +13,10 @@ public class GlobalControllerAdvice extends ResponseEntityExceptionHandler {
   @ExceptionHandler(IdNotFoundException.class)
   public ProblemDetail handleIdNotFound() {
     return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Wrong Id");
+  }
+
+  @ExceptionHandler(CustomerException.class)
+  public ProblemDetail handleCustomerException() {
+    return ProblemDetail.forStatusAndDetail(HttpStatus.ALREADY_REPORTED, "Error in Customers");
   }
 }
