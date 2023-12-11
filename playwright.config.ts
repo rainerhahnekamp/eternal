7184f5ece9c28927dacb22057404709cdccb34fe
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-12-11 17:18:46 +0100

    feat: add basic test

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,13 +21,14 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'http://localhost:4200',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
--- tests/check.spec.ts
+++ tests/check.spec.ts
@@ -1,8 +1,25 @@
 import { expect, test } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('');
-  await expect(
-    page.getByRole('heading', { name: 'Welcome to Eternal' }),
-  ).toBeVisible();
+test.describe('init', () => {
+  test.only('request brochure', async ({ page }) => {
+    await page.goto('');
+    await page.getByRole('link', { name: 'Holidays', exact: true }).click();
+    await expect(
+      page.locator('app-holiday-card').filter({ hasText: 'Vienna' }),
+    ).toBeVisible();
+    await page
+      .locator('app-holiday-card')
+      .filter({ hasText: 'Vienna' })
+      .getByTestId('btn-brochure')
+      .click();
+
+    await page.getByTestId('address').fill('Domgasse 5');
+    await page.getByTestId('btn-search').click();
+    await expect(page.getByTestId('lookup-result')).toHaveText('Brochure sent');
+  });
+
+  test('has title', async ({ page }) => {
+    await page.goto('');
+    await page.locator('div');
+  });
 });
