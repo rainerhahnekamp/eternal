a01b14a55fbdc6948f54a7a1bf52cbda45b1367c
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2024-02-01 18:40:29 +0100

    feat: add further tests

--- tests/init.spec.ts
+++ tests/init.spec.ts
@@ -1,6 +1,19 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('');
-  await page.getByTestId('btn-customers').click();
+test.describe('init', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('');
+    await page.getByText('Application is ready').waitFor();
+  });
+
+  test('test', async ({ page }) => {
+    await page.getByRole('link', { name: 'Holidays', exact: true }).click();
+    await page
+      .getByLabel('Vienna')
+      .getByRole('link', { name: 'Get a Brochure' })
+      .click();
+    await page.getByLabel('Address').fill('Domgasse 5');
+    await page.getByRole('button', { name: 'Send' }).click();
+    await expect(page.getByText('Brochure sent')).toBeVisible();
+  });
 });
