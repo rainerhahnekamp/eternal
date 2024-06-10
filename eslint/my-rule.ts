554b0fc4baf47d23fa53259dd442249a5c9963e8
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2024-06-10 13:52:11 +0200

    feat: add basic rule

---
+++ eslint/my-rule.ts
@@ -0,0 +1,45 @@
+import { ESLintUtils } from '@typescript-eslint/utils';
+
+const createRule = ESLintUtils.RuleCreator(
+  (name) => `https://example.com/rule/${name}`,
+);
+
+// Type: RuleModule<"uppercase", ...>
+export const upperCaseRule = createRule({
+  name: 'app/uppercase',
+  create(context) {
+    return {
+      FunctionDeclaration(node) {
+        if (node.id != null) {
+          if (/^[a-z]/.test(node.id.name)) {
+            context.report({
+              messageId: 'uppercase',
+              node: node.id,
+            });
+          }
+        }
+      },
+    };
+  },
+  meta: {
+    docs: {
+      description:
+        'Function declaration names should start with an upper-case letter.',
+    },
+    messages: {
+      uppercase: 'Start this name with an upper-case letter.',
+    },
+    type: 'suggestion',
+    schema: [],
+  },
+  defaultOptions: [],
+});
+
+export default {
+  meta: {},
+  configs: {},
+  rules: {
+    'app/uppercase': upperCaseRule,
+  },
+  processors: {},
+};
