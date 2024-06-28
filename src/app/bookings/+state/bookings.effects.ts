0e14db51f6fefa9243f6dfa781c0cb809987c6d5
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2024-06-28 10:05:00 +0200

    chore: update ngrx

--- src/app/bookings/+state/bookings.effects.ts
+++ src/app/bookings/+state/bookings.effects.ts
@@ -1,5 +1,6 @@
 import { inject, Injectable } from '@angular/core';
-import { Actions, concatLatestFrom, createEffect, ofType } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { concatLatestFrom } from '@ngrx/operators';
 import { filter, map } from 'rxjs';
 import { bookingsActions } from './bookings.actions';
 import { Booking } from './bookings.reducer';
