ebb2d6d8489a283040788d22c93b1905ba3b8f38
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2024-10-28 11:02:53 +0100

    feat: remove tests

--- cypress/e2e/misc.cy.ts
+++
@@ -1,8 +0,0 @@
-describe('init', () => {
-  beforeEach(() => {
-    cy.visit('');
-    cy.testid('hydrated').should('contain.text', 'Application is ready');
-  });
-
-  it('should do a sanity check', () => {});
-});
