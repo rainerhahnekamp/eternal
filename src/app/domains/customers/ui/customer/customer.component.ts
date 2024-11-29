32ab31b2fe7076e0a5b593bb3f626ebae24467ce
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2024-11-29 14:27:49 +0100

    feat: switch to td form

--- src/app/domains/customers/ui/customer/customer.component.ts
+++ src/app/domains/customers/ui/customer/customer.component.ts
@@ -1,16 +1,5 @@
-import {
-  Component,
-  effect,
-  EventEmitter,
-  inject,
-  input,
-  Output,
-} from '@angular/core';
-import {
-  NonNullableFormBuilder,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { Component, input, linkedSignal, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { RouterLinkWithHref } from '@angular/router';
@@ -18,54 +7,40 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { Customer } from '../../model/customer';
 import { SelectOptions } from '../../../../shared/form/select-options';
+import { FormErrorsComponent } from '../../../../shared/form/form-errors.component';
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
   styleUrls: ['./customer.component.scss'],
   imports: [
-    ReactiveFormsModule,
     MatButtonModule,
     MatDatepickerModule,
     RouterLinkWithHref,
     MatInputModule,
     MatSelectModule,
+    FormErrorsComponent,
+    FormsModule,
   ],
 })
 export class CustomerComponent {
   customer = input.required<Customer>();
   countries = input.required<SelectOptions>();
   showDeleteButton = input.required<boolean>();
-  @Output() save = new EventEmitter<Customer>();
-  @Output() remove = new EventEmitter<Customer>();
+  save = output<Customer>();
+  remove = output<Customer>();
+  formCustomer = linkedSignal(this.customer);
 
-  constructor() {
-    effect(() => {
-      this.formGroup.setValue(this.customer());
-    });
-  }
-
-  formGroup = inject(NonNullableFormBuilder).group({
-    id: [0],
-    firstname: ['', [Validators.required]],
-    name: ['', [Validators.required]],
-    country: ['', [Validators.required]],
-    birthdate: ['', [Validators.required]],
-  });
   submit() {
-    if (this.formGroup.valid) {
-      this.save.emit(this.formGroup.getRawValue());
+    if (this.formCustomer) {
+      this.save.emit(this.formCustomer());
     }
   }
 
   handleRemove() {
-    if (
-      this.customer &&
-      confirm(
-        `Really delete ${this.customer().firstname} ${this.customer().name}?`,
-      )
-    ) {
-      this.remove.emit(this.customer());
+    const { firstname, name } = this.formCustomer();
+    if (confirm(`Really delete ${firstname} ${name}?`)) {
+      this.remove.emit(this.formCustomer());
     }
   }
 }
