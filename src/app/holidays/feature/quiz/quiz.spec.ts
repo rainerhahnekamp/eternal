61183ff9fdae7e0985e61f371337f365b91c2837
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2024-12-06 07:34:35 +0100

    feat: add missing async

--- src/app/holidays/feature/quiz/quiz.spec.ts
+++ src/app/holidays/feature/quiz/quiz.spec.ts
@@ -1,5 +1,5 @@
 describe('Quiz Service', () => {
-  it('should show that question was answered success', () => {
+  it('should show that question was answered success', async () => {
     expect(true).toBe(true);
   });
 });
