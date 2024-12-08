41f4aeb961b82b84b9b232155cf68e97188a7eda
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2024-12-08 21:46:22 +0100

    feat: remote NgRx store tests

--- src/app/holidays/feature/+state/test-utils.ts
+++
@@ -1,6 +0,0 @@
-import { HolidaysState } from '@app/holidays/feature/+state/holidays.reducer';
-
-export const createState = (values: Partial<HolidaysState> = {}) => ({
-  ...{ holidays: [], favouriteIds: [] },
-  ...values,
-});
