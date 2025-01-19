26e917d502f75c535886546915e341adc2a7fa07
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2025-01-19 10:26:29 +0100

    fix: tests

--- tests/init.spec.ts
+++ tests/init.spec.ts
@@ -2,6 +2,7 @@ import { expect, test } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('http://localhost:4200/');
+  await page.getByText('Application is ready').waitFor();
   await page.getByTestId('btn-customers').click();
   await expect(page.getByRole('heading', { name: 'Customer' })).toBeVisible();
 });
