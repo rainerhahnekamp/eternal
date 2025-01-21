e5696c0b032a9c90caa3df1a27d00a6d73e8c757
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2025-01-22 00:20:57 +0100

    feat: add search

--- src/app/holidays/feature/holidays/holidays.component.ts
+++ src/app/holidays/feature/holidays/holidays.component.ts
@@ -1,14 +1,30 @@
 import { Component, inject } from '@angular/core';
-import { Store } from '@ngrx/store';
 import { HolidayCardComponent } from '@app/holidays/ui';
 import { Holiday } from '@app/holidays/model';
-import { fromHolidays, holidaysActions } from '@app/holidays/data';
+import { FormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { HolidaysService, HolidayWithFavourite } from './holidays.service';
 
 @Component({
   selector: 'app-holidays',
   template: `<h2>Choose among our Holidays</h2>
+    <form (ngSubmit)="search()">
+      <mat-form-field>
+        <mat-label>Name</mat-label>
+        <input [(ngModel)]="name" name="name" matInput />
+      </mat-form-field>
+
+      <mat-form-field>
+        <mat-label>Description</mat-label>
+        <input [(ngModel)]="description" name="description" matInput />
+      </mat-form-field>
+
+      <button mat-raised-button>Search</button>
+    </form>
+
     <div class="flex flex-wrap justify-evenly">
-      @for (holiday of holidays(); track byId($index, holiday)) {
+      @for (holiday of holidays; track byId($index, holiday)) {
         <app-holiday-card
           [holiday]="holiday"
           (addFavourite)="addFavourite($event)"
@@ -17,21 +33,31 @@ import { fromHolidays, holidaysActions } from '@app/holidays/data';
         </app-holiday-card>
       }
     </div> `,
-  imports: [HolidayCardComponent],
+  imports: [HolidayCardComponent, FormsModule, MatInputModule, MatButton],
 })
 export class HolidaysComponent {
-  #store = inject(Store);
-  holidays = this.#store.selectSignal(fromHolidays.selectHolidaysWithFavourite);
+  name = '';
+  description = '';
+  holidays = [] as HolidayWithFavourite[];
+
+  #holidaysService = inject(HolidaysService);
 
   addFavourite(id: number) {
-    this.#store.dispatch(holidaysActions.addFavourite({ id }));
+    // this.#holidaysStore.addFavourite(id);
   }
 
   removeFavourite(id: number) {
-    this.#store.dispatch(holidaysActions.removeFavourite({ id }));
+    // this.#holidaysStore.removeFavourite(id);
   }
 
   byId(index: number, holiday: Holiday) {
     return holiday.id;
+  }
+
+  async search() {
+    this.holidays = await this.#holidaysService.findByPromise(
+      this.name,
+      this.description,
+    );
   }
 }
