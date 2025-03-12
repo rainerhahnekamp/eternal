7c2c9f2c31d4bf0acfb1a210a86dd9958258372f
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2025-03-12 18:41:19 +0100

    refactor: add testing library

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    }
+    },
 
     /* Test against mobile viewports. */
     // {
@@ -59,9 +59,4 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npm run start',
-    url: 'http://localhost:4200',
-    reuseExistingServer: !process.env.CI,
-  },
 });
