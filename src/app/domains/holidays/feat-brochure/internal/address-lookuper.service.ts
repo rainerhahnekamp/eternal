c2cf940e9f919bc16f969201319461074e3b85e1
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2025-04-06 15:14:01 +0200

    feat: use resource for address lookuper

--- src/app/domains/holidays/feat-brochure/internal/address-lookuper.service.ts
+++ src/app/domains/holidays/feat-brochure/internal/address-lookuper.service.ts
@@ -1,19 +1,29 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { parseAddress } from './parse-address';
+import { httpResource } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { z } from 'zod';
+
+const addressResponseSchema = z.array(z.unknown());
 
 @Injectable({ providedIn: 'root' })
 export class AddressLookuper {
-  #httpClient = inject(HttpClient);
+  lookup(query: () => string) {
+    return httpResource(
+      () => {
+        if (!query()) {
+          return undefined;
+        }
 
-  lookup(query: string): Observable<boolean> {
-    parseAddress(query);
-    return this.#httpClient
-      .get<unknown[]>('https://nominatim.openstreetmap.org/search.php', {
-        params: new HttpParams().set('format', 'jsonv2').set('q', query),
-      })
-      .pipe(map((addresses) => addresses.length > 0));
+        return {
+          url: `https://nominatim.openstreetmap.org/search.php`,
+          params: { format: 'jsonv2', q: query() },
+        };
+      },
+      {
+        parse: (response) => {
+          const result = addressResponseSchema.safeParse(response);
+          return result.success ? result.data.length > 0 : false;
+        },
+      },
+    );
   }
 }
--- src/app/domains/holidays/feat-brochure/request-brochure.component.ts
+++ src/app/domains/holidays/feat-brochure/request-brochure.component.ts
@@ -1,12 +1,16 @@
-import { Component, effect, inject, input, signal } from '@angular/core';
-import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { lastValueFrom } from 'rxjs';
+import { Component, computed, inject, signal } from '@angular/core';
+import {
+  AbstractControl,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { AddressLookuper } from './internal/address-lookuper.service';
 import { MatFormField, MatHint, MatLabel } from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
 import { MatInput } from '@angular/material/input';
 import { MatAnchor, MatButton } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import { isValidAddress } from './internal/is-valid-address';
 
 @Component({
   selector: 'app-request-info',
@@ -24,30 +28,31 @@ import { RouterLink } from '@angular/router';
   ],
 })
 export class RequestBrochureComponent {
-  #formBuilder = inject(NonNullableFormBuilder);
-  #lookuper = inject(AddressLookuper);
+  readonly #formBuilder = inject(NonNullableFormBuilder);
+  readonly #address = signal('');
+  readonly #isExistingAddress = inject(AddressLookuper).lookup(this.#address);
+  protected readonly lookupResult = computed(() => {
+    const isExistingAddress = this.#isExistingAddress.value();
+    return isExistingAddress === undefined
+      ? ''
+      : isExistingAddress
+        ? 'Brochure sent'
+        : 'Address not found';
+  });
 
   formGroup = this.#formBuilder.group({
-    address: [''],
+    address: [
+      '',
+      (ac: AbstractControl) =>
+        isValidAddress(ac.value) ? null : { invalidAddress: true },
+    ],
   });
-  title = 'Request More Information';
-  address = input('');
-  lookupResult = signal('');
-
-  constructor() {
-    effect(() => {
-      const address = this.address();
-      if (!address) {
-        return;
-      }
-      this.formGroup.patchValue({ address });
-    });
-  }
 
   async search() {
-    const found = await lastValueFrom(
-      this.#lookuper.lookup(this.formGroup.getRawValue().address),
-    );
-    this.lookupResult.set(found ? 'Brochure sent' : 'Address not found');
+    if (this.formGroup.invalid) {
+      return;
+    }
+
+    this.#address.set(this.formGroup.getRawValue().address);
   }
 }
