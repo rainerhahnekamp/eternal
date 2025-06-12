c4e8729dc267d3e716836550426891269a844c59
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2025-06-12 13:42:10 +0200

    feat: add solution

---
+++ src/app/domains/holidays/feat-quiz/quiz-store.spec.ts
@@ -0,0 +1,51 @@
+import { TestBed } from '@angular/core/testing';
+import { QuizStore } from './quiz-store';
+import { provideHttpClient } from '@angular/common/http';
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from '@angular/common/http/testing';
+import { provideZonelessChangeDetection } from '@angular/core';
+import { createQuiz } from './tests/create-quiz';
+import { assertDefined } from '../../../shared/util/assert-defined';
+
+describe('QuizStore', () => {
+  it('should be check for correct and incorrect', async () => {
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideZonelessChangeDetection(),
+      ],
+    });
+
+    const quiz = createQuiz(1);
+    const rightAnswer = quiz.questions[0].answers.find((a) => a.isCorrect)?.id;
+    const wrongAnswer = quiz.questions[0].answers.find((a) => !a.isCorrect)?.id;
+    assertDefined(rightAnswer);
+    assertDefined(wrongAnswer);
+
+    const store = TestBed.inject(QuizStore);
+    store.loadQuiz(1);
+    await new Promise((resolve) => setTimeout(resolve));
+    const ctrl = TestBed.inject(HttpTestingController);
+
+    ctrl.expectOne('/holiday/1/quiz').flush(quiz);
+    await new Promise((resolve) => setTimeout(resolve));
+    expect(store.questions().length).toBe(2);
+
+    store.handleAnswer({ questionId: 1, choiceId: rightAnswer });
+    expect(store.quizStatus()).toEqual({
+      correct: 1,
+      incorrect: 0,
+      unanswered: 1,
+    });
+
+    store.handleAnswer({ questionId: 1, choiceId: wrongAnswer });
+    expect(store.quizStatus()).toEqual({
+      correct: 0,
+      incorrect: 1,
+      unanswered: 1,
+    });
+  });
+});
---
+++ src/app/domains/holidays/feat-quiz/tests/quiz.spec.ts
@@ -0,0 +1,56 @@
+import { TestBed } from '@angular/core/testing';
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideLocationMocks } from '@angular/common/testing';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { RouterTestingHarness } from '@angular/router/testing';
+import { QuizStore } from '../quiz-store';
+import { createQuiz } from './create-quiz';
+import { screen, within } from '@testing-library/angular';
+import { QuizPage } from '../quiz-page';
+import { provideZonelessChangeDetection } from '@angular/core';
+
+describe('Quiz Feature', () => {
+  it('should show correct status', async () => {
+    TestBed.configureTestingModule({
+      imports: [QuizPage],
+      providers: [
+        provideZonelessChangeDetection(),
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideLocationMocks(),
+        provideRouter(
+          [
+            {
+              path: 'quiz/:id',
+              component: QuizPage,
+            },
+          ],
+          withComponentInputBinding(),
+        ),
+        QuizStore,
+      ],
+    });
+
+    const harness = await RouterTestingHarness.create('quiz/1');
+    const ctrl = TestBed.inject(HttpTestingController);
+    ctrl.expectOne('/holiday/1/quiz').flush(createQuiz(1));
+    await harness.fixture.whenStable();
+    harness.fixture.detectChanges();
+
+    // screen.getByRole
+    const question = screen
+      .getAllByLabelText('question')
+      .find((question) => within(question).getByText(/programming language/));
+    if (!question) {
+      throw 'nix geht';
+    }
+    within(question).getByRole('button', { name: 'TypeScript' }).click();
+
+    await harness.fixture.whenStable();
+    expect(screen.getByText('Correct: 1')).toBeDefined();
+  });
+});
