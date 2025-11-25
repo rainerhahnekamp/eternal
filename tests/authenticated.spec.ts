a25ca6b9cb5b11a23bbba71cb67da51d3acaaa8d
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2025-11-25 10:20:34 +0100

    fix: authentication

--- tests/authenticated.spec.ts
+++ tests/authenticated.spec.ts
@@ -1,8 +1,8 @@
 import { expect, test } from '@playwright/test';
 import { storagePath } from './storage-path';
 
+test.use({ storageState: storagePath });
 test.describe('Authenticated', () => {
-  test.use({ storageState: storagePath });
   test('user should be authenticated', async ({ page }) => {
     await page.goto('');
     await expect(page.getByText('Welcome John List')).toBeVisible();
