c7f852a42bddfcec7fecb9e862c0462c7fe60d44
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2026-04-07 16:23:04 +0200

    feat: add-profiler-proxy

---
+++ src/app/shared/util/add-profiler-proxy.ts
@@ -0,0 +1,28 @@
+export function addProfilerProxy(
+  fn: (...args: unknown[]) => void,
+  name: string,
+) {
+  new Proxy(fn, {
+    apply(target, thisArg, argArray: unknown[]) {
+      const startedAt = performance.now();
+
+      const result = Reflect.apply(target, thisArg, argArray);
+      const end = performance.now();
+      const duration = end - startedAt;
+
+      console.group('profiler', String(name));
+      console.info(
+        `Start: ${new Date(performance.timeOrigin + startedAt).toLocaleTimeString()}`,
+      );
+      console.info(
+        `End: ${new Date(performance.timeOrigin + end).toLocaleTimeString()}`,
+      );
+      console.info(`Duration: ${duration}ms`);
+      console.log('Parameters: %o', argArray);
+      console.info('Returned: %o', result);
+      console.groupEnd();
+
+      return result;
+    },
+  });
+}
