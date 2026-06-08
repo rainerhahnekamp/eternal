42ec44de4235133bcf0908056af661e52bc76cad
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2026-06-08 22:18:43 +0200

    feat: finalize update

--- src/app/domains/holidays/feat-brochure/request-brochure-page.ts
+++ src/app/domains/holidays/feat-brochure/request-brochure-page.ts
@@ -4,43 +4,24 @@ import {
   computed,
   signal,
 } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import {
-  MatError,
-  MatFormField,
-  MatHint,
-  MatLabel,
-} from '@angular/material/form-field';
-import { MatIcon } from '@angular/material/icon';
-import { MatInput } from '@angular/material/input';
+
 import { MatAnchor, MatButton } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
-import { FormField, form, submit, validate } from '@angular/forms/signals';
+import { form, validate, FormRoot } from '@angular/forms/signals';
 import { isValidAddress } from './internal/is-valid-address';
+import { TextFieldComponent } from '../../../shared/form/text-field';
 
 @Component({
   selector: 'app-request-info',
   templateUrl: './request-brochure-page.html',
-  imports: [
-    ReactiveFormsModule,
-    MatError,
-    MatFormField,
-    MatIcon,
-    MatLabel,
-    MatInput,
-    MatButton,
-    MatHint,
-    RouterLink,
-    MatAnchor,
-    FormField,
-  ],
+  imports: [FormRoot, MatButton, RouterLink, MatAnchor, TextFieldComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RequestBrochurePage {
-  readonly #address = signal('');
-  readonly #isExistingAddress = { value: () => true };
+  private readonly isExistingAddress = { value: () => true };
+
   protected readonly lookupResult = computed(() => {
-    const isExistingAddress = this.#isExistingAddress.value();
+    const isExistingAddress = this.isExistingAddress.value();
     return isExistingAddress === undefined
       ? ''
       : isExistingAddress
@@ -48,20 +29,13 @@ export class RequestBrochurePage {
         : 'Address not found';
   });
 
-  readonly #addressModel = signal({ address: '' });
+  private readonly addressModel = signal({ address: '', count: 1 });
 
-  addressForm = form(this.#addressModel, (path) => {
+  protected readonly addressForm = form(this.addressModel, (path) => {
     validate(path.address, (ctx) => {
       return isValidAddress(ctx.value())
         ? null
         : { kind: 'invalidAddress', message: 'Address is invalid' };
     });
   });
-
-  search(event: Event) {
-    event.preventDefault();
-    void submit(this.addressForm, async () =>
-      this.#address.set(this.addressForm.address().value()),
-    );
-  }
 }
